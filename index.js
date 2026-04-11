5553aa892bb60a25326edf10f38a7cc21ac05b8c
vais2004 <vkawale2004@gmail.com>
2026-04-11 22:42:12 +0530

    update feat: wrote - multer - api endpoint - upload to cloudinary - save to mongodb

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cloudinary = require("cloudinary");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { ImageModel } = require("./models/images");
 
 dotenv.config();
 const app = express();
@@ -17,4 +18,34 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+//multer
+const storage = multer.diskStorage({});
+const upload = multer({ storage });
+
+//api endpoint
+app.post("/upload", upload.single("image"), async (req, res) => {
+  try {
+    const file = req.file;
+    if (!file) {
+      return res.status(400).send("No file uploaded");
+    }
+    //upload to cloudinary
+    const result = await cloudinary.uploader.upload(file.path, {
+      folder: "uploads",
+    });
+    //save to mongodb
+    const newImage = new ImageModel({ imageUrl: result.secure_url });
+    await newImage.save;
+
+    res.status(200).json({
+      message: "Image uploaded successfully",
+      imageUrl: result.secure_url,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error occure while uploading the image", error });
+  }
 });
