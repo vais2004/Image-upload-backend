6e6f60e8c2f559fda9ff66eabed485aa99c7fc69
vais2004 <vkawale2004@gmail.com>
2026-04-11 18:21:06 +0530

    update: feat CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_APT_SECRET

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 initializeDatabase();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
